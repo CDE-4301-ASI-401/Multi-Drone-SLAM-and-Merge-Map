797778084f66abe168a52aa328f9dae631e04db7
yanyew-low <lowyanyew@gmail.com>
2025-02-14 20:07:19 +0800

    test

---
+++ src/crazyswarm2/crazyflie/launch/default_swarm_map.launch.py
@@ -0,0 +1,131 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import yaml
+
+
+def generate_launch_description():
+
+    # load crazyflies
+    crazyflies_yaml = os.path.join(
+        get_package_share_directory('crazyflie'),
+        'config',
+        'crazyflies.yaml')
+
+    with open(crazyflies_yaml, 'r') as ymlfile:
+        crazyflies = yaml.safe_load(ymlfile)
+
+    server_params = crazyflies
+
+    # robot description
+    urdf = os.path.join(
+        get_package_share_directory('crazyflie'),
+        'urdf',
+        'crazyflie_description.urdf')
+    with open(urdf, 'r') as f:
+        robot_desc = f.read()
+    server_params['robot_description'] = robot_desc
+
+#################
+    server_yaml = os.path.join(
+        get_package_share_directory('crazyflie'),
+        'config',
+        'server.yaml')
+    with open(server_yaml, 'r') as ymlfile:
+        server_yaml_contents = yaml.safe_load(ymlfile)
+    server_params = [crazyflies] + [server_yaml_contents["/crazyflie_server"]["ros__parameters"]]
+#################
+
+    crazyflie_name = '/cf231'
+
+    return LaunchDescription([
+      # These nodes need https://github.com/IMRCLab/crazyswarm2
+        Node(
+            package='crazyflie',
+            executable='crazyflie_server.py',
+            name='crazyflie_server',
+            output='screen',
+            parameters=[server_params],
+            #remappings=[
+            #    ('/cf09/odom', '/cfcombi/odom'),
+            #    ('/cf09/scan', '/cfcombi/scan'),
+            #    ('/cf14/odom', '/cfcombi/odom'),
+            #    ('/cf14/scan', '/cfcombi/scan'),
+            #    ('/cf6/odom', '/cfcombi/odom'),
+            #    ('/cf6/scan', '/cfcombi/scan'),
+            #]
+        ),
+        Node(
+            package='crazyflie',
+            executable='vel_mux.py',
+            name='vel_mux',
+            output='screen',
+            parameters=[{'hover_height': 0.3},
+                        {'incoming_twist_topic': '/cmd_vel'},
+                        {'robot_prefix': 'cf09'}]
+        ),
+        Node(
+            package='crazyflie',
+            executable='vel_mux.py',
+            name='vel_mux',
+            output='screen',
+            parameters=[{'hover_height': 0.4},
+                        {'incoming_twist_topic': '/cmd_vel'},
+                        {'robot_prefix': 'cf14'}]
+        ),
+        Node(
+            package='crazyflie',
+            executable='vel_mux.py',
+            name='vel_mux',
+            output='screen',
+            parameters=[{'hover_height': 0.5},
+                        {'incoming_twist_topic': '/cmd_vel'},
+                        {'robot_prefix': 'cf6'}]
+        ),
+        Node(
+            package='crazyflie',
+            executable='simple_mapper_multiranger.py',
+            name='simple_mapper_multiranger',
+            output='screen',
+            parameters=[
+                {'robot_prefix': 'cf09'}]
+        ),
+        Node(
+            package='crazyflie',
+            executable='simple_mapper_multiranger.py',
+            name='simple_mapper_multiranger',
+            output='screen',
+            parameters=[
+                {'robot_prefix': 'cf14'}]
+        ),
+        Node(
+            package='crazyflie',
+            executable='simple_mapper_multiranger.py',
+            name='simple_mapper_multiranger',
+            output='screen',
+            parameters=[
+                {'robot_prefix': 'cf6'}]
+        ),
+      # these node need: https://github.com/abdulkadrtr/mapMergeForMultiRobotMapping-ROS2
+        Node(
+            package='merge_map',
+            executable='merge_map',
+            output='screen',
+            remappings=[
+                ("/map1", "/cf6/map"),
+                ("/map2", "/cf14/map"),
+                ("/merge_map", "/map12")
+            ],
+        ),
+        Node(
+            package='merge_map',
+            executable='merge_map',
+            output='screen',
+            remappings=[
+                ("/map1", "/map12"),
+                ("/map2", "/cf09/map"),
+           ],
+        ),
+    ])
